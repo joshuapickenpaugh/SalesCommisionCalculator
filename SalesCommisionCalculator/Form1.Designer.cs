968b401bd3b94b86e4e974300935f802e1381961
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-07-24 12:44:47 -0500

    Final Commit.

--- SalesCommisionCalculator/Form1.Designer.cs
+++ SalesCommisionCalculator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SalesCommisionCalculator
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,32 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmbItem1 = new System.Windows.Forms.ComboBox();
+            this.cmbItem2 = new System.Windows.Forms.ComboBox();
+            this.cmbItem3 = new System.Windows.Forms.ComboBox();
+            this.cmbItem4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cmbItem1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
+            this.cmbItem1.FormattingEnabled = true;
+            this.cmbItem1.Location = new System.Drawing.Point(69, 22);
+            this.cmbItem1.Name = "cmbItem1";
+            this.cmbItem1.Size = new System.Drawing.Size(110, 21);
+            this.cmbItem1.TabIndex = 0;
             // 
-            // Form1
+            // cmbItem2
+            // 
+            this.cmbItem2.FormattingEnabled = true;
+            this.cmbItem2.Location = new System.Drawing.Point(70, 50);
+            this.cmbItem2.Name = "cmbItem2";
+            this.cmbItem2.Size = new System.Drawing.Size(110, 21);
+            this.cmbItem2.TabIndex = 1;
+            // 
+            // cmbItem3
+            // 
+            this.cmbItem3.FormattingEnabled = true;
+            this.cmbItem3.Location = new System.Drawing.Point(69, 78);
+            this.cmbItem3.Name = "cmbItem3";
+            this.cmbItem3.Size = new System.Drawing.Size(110, 21);
+            this.cmbItem3.TabIndex = 2;
+            // 
+            // cmbItem4
+            // 
+            this.cmbItem4.FormattingEnabled = true;
+            this.cmbItem4.Location = new System.Drawing.Point(69, 106);
+            this.cmbItem4.Name = "cmbItem4";
+            this.cmbItem4.Size = new System.Drawing.Size(110, 21);
+            this.cmbItem4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Item 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Item 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(185, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Item 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(185, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Item 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(105, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "This week\'s pay:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotal.Location = new System.Drawing.Point(72, 195);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(150, 63);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(70, 133);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(152, 23);
+            this.btnCalc.TabIndex = 10;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(564, 267);
-            this.Controls.Add(this.comboBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(290, 267);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbItem4);
+            this.Controls.Add(this.cmbItem3);
+            this.Controls.Add(this.cmbItem2);
+            this.Controls.Add(this.cmbItem1);
+            this.Name = "frmMain";
+            this.Text = "Sales Commission Calculator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmbItem1;
+        private System.Windows.Forms.ComboBox cmbItem2;
+        private System.Windows.Forms.ComboBox cmbItem3;
+        private System.Windows.Forms.ComboBox cmbItem4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnCalc;
     }
 }
 
--- SalesCommisionCalculator/Form1.cs
+++ SalesCommisionCalculator/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Joshua Pickenpaugh
+//July 24th, 2017
+//Commission Calculator
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +14,77 @@ using System.Windows.Forms;
 
 namespace SalesCommisionCalculator
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+
+            //Populates combo boxes for height and weight...:     
+            //Height:
+            for (int i = 0; i < 9999; i++)
+            {
+                cmbItem1.Items.Add(i);
+            }
+
+            for (int i = 0; i < 9999; i++)
+            {
+                cmbItem2.Items.Add(i);
+            }
+
+            for (int i = 0; i < 9999; i++)
+            {
+                cmbItem3.Items.Add(i);
+            }
+
+            for (int i = 0; i < 9999; i++)
+            {
+                cmbItem4.Items.Add(i);
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //No code here.
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double dblItem1;
+            double dblItem2;
+            double dblItem3;
+            double dblItem4;
+            double dblTotal;
+
+            const double ITEM_PRICE_1 = 239.99;
+            const double ITEM_PRICE_2 = 129.75;
+            const double ITEM_PRICE_3 = 99.95;
+            const double ITEM_PRICE_4 = 350.89;
+            const double WEEKLY_BASE_PAY = 200.00;
+
+            //Gets selected item from combo boxes, assigns it to 
+            //a variable:
+            Object selectedItem1 = cmbItem1.SelectedItem;
+            dblItem1 = Convert.ToDouble(selectedItem1.ToString());
+
+            Object selectedItem2 = cmbItem1.SelectedItem;
+            dblItem2 = Convert.ToDouble(selectedItem2.ToString());
+
+            Object selectedItem3 = cmbItem1.SelectedItem;
+            dblItem3 = Convert.ToDouble(selectedItem3.ToString());
+
+            Object selectedItem4 = cmbItem1.SelectedItem;
+            dblItem4 = Convert.ToDouble(selectedItem4.ToString());
+
+            //Calculates:
+            dblItem1 = (dblItem1 * ITEM_PRICE_1) * .09;
+            dblItem2 = (dblItem2 * ITEM_PRICE_2) * .09;
+            dblItem3 = (dblItem3 * ITEM_PRICE_3) * .09;
+            dblItem4 = (dblItem4 * ITEM_PRICE_4) * .09;
+
+            dblTotal = (dblItem1 + dblItem2 + dblItem3 + dblItem4) + WEEKLY_BASE_PAY;
+
+            lblTotal.Text = Convert.ToString(dblTotal.ToString("N2"));
         }
     }
 }
